fedd62be8904368315633604bd3bc60dd0626ff7
mid <mid@KCSK3333.k303.kcsk.ac.jp>
2026-06-01 13:49:54 +0900

    start

---
+++ src/test/java/com/example/demo/TrainingAccountItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAccountItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
